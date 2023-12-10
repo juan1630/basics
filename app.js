21c7b6c2497c8b8a5d1b4b20dab53fa59563459d
Juan Patrón <josejuanpatron1630@gmail.com>
2023-12-10 17:52:38 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// split solo para strings
+console.log("hola mundo".split("")); // regresa un arreglo con todas las letras separadas dentro de un array = ["h","o", "l", "a"]
+console.log("hola mundo".split(" ")); // regresa un arreglo con las palabras separadas por un espacio = ["hola", "mundo"]
+
+
+const sliced = ["hola", "mundo","adios", "mundo"].slice(0); 
+// slice corta el areglo, neesita 2 parametros el primero es de donde inicia a 
+//cortar el aargelo y el segundo la cantidad de elementos que va a cortar
+console.log( sliced , "slice");
