2d885eb9d42a5a8654875ba70267f2339bae83f0
Juan Patrón <josejuanpatron1630@gmail.com>
2023-12-10 18:32:34 -0600

    Strings methods and array

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-// split solo para strings
-console.log("hola mundo".split("")); // regresa un arreglo con todas las letras separadas dentro de un array = ["h","o", "l", "a"]
-console.log("hola mundo".split(" ")); // regresa un arreglo con las palabras separadas por un espacio = ["hola", "mundo"]
-
-
-const sliced = ["hola", "mundo","adios", "mundo"].slice(0); 
+const sliced = ["hola", "mundo","adios", "mundo"]; 
 // slice corta el areglo, neesita 2 parametros el primero es de donde inicia a 
 //cortar el aargelo y el segundo la cantidad de elementos que va a cortar
 console.log( sliced , "slice");
+
+
+// El metodo splice borra elementos del arreglo o los agrega, no regresa un nuevo arreglo
+const months = ['Jan', 'March', 'April', 'June'];
+
+console.log(months);
+
+//agrega un elemento nuevo al arreglo
+months.splice(1, 0, 'Feb');
+
+console.log(months);
+months.splice(0,1)
+
+console.log(months);
+
+
+
+let months2 = ['Jan', 'March', 'April', 'June']
+let months3 = months2;
+//No se crea un nuevo array, se crea una referencia a este array
+months2 = ['hola', "mundo"];
+
+console.log(months2, "month2") // output months2 ["hola", "mundo"]
+console.log(months3, "months3") // outpt=ut minth3 ["jan","march", "april"]
+
+
+const months4 = ['Jan', 'March', 'April', 'June']
+const months5 = months4;
+months4.push("hola")
+
+console.log(months4) //["hola", "mundo"]
+console.log(months5) // ['Jan', 'March', 'April', 'June']
+
---
+++ strings.js
@@ -0,0 +1,7 @@
+// split solo para strings
+console.log("hola mundo".split("")); // regresa un arreglo con todas las letras separadas dentro de un array = ["h","o", "l", "a"]
+console.log("hola mundo".split(" ")); // regresa un arreglo con las palabras separadas por un espacio = ["hola", "mundo"]
+
+
+//Este metodo extrea elementos de la cadena y los regresa en un string
+console.log( "Hola mundo curel y adios".slice(0,2)); //output Ho
